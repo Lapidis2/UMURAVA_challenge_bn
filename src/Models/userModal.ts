42fb5311d173c695f578c85b75a7c204d637e3e4
lapidis2 <hitlapidis2@gmail.com>
2024-06-24 15:28:35 +0200

    new added files

--- src/Models/userModal.ts
+++ src/Models/userModal.ts
@@ -25,7 +25,7 @@ const userSchema=new mongoose.Schema({
     },
     role:{
         type:String,
-        default:'Guest'
+        default:'Guest'||'Admin'
     },
     createdAt:{
         type:Date,
