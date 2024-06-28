f2d79c4c51279267c2ddae2f45b9a5b15477b90b
HITAYEZU Jean Pierre <hitlapidis2@gmail.com>
2024-06-29 00:51:52 +0200

    new

--- src/index.ts
+++ src/index.ts
@@ -13,6 +13,7 @@ import http from "http"
 import dotenv from "dotenv"
 dotenv.config()
 const app = express()
+app.use(cors())
 const server = http.createServer(app);
 const io = new Server(server,{
   cors:{
