0fbf47bf685992bbe2578f79f680d481f644be33
HITAYEZU Jean Pierre <hitlapidis2@gmail.com>
2024-06-28 11:28:18 +0200

    new commit

--- src/index.ts
+++ src/index.ts
@@ -16,8 +16,9 @@ const app = express()
 const server = http.createServer(app);
 const io = new Server(server,{
   cors:{
-    origin: "http://localhost:3000/api",
-    methods: ["GET","POST"]
+    origin: "*",
+    methods: ["GET","POST"],
+    credentials:true
   }
 })
 
