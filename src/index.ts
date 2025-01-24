3d8e2dd9b304664677631bb11bd3fbea60fc9d2e
HITAYEZU Jean Pierre <hitlapidis2@gmail.com>
2025-01-24 13:03:07 +0200

    new changes

--- src/index.ts
+++ src/index.ts
@@ -19,7 +19,7 @@ app.use(cors())
 const server = http.createServer(app);
 const io = new Server(server,{
   cors:{
-    origin: "*",
+    origin: ["http://localhost:3000", "https://jeanpierreportfolio.netlify.app/"],
     methods: ["GET","POST"],
   
   }
