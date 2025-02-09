8e3f1821a942a4cba3c4b46279258c7311df3bd0
HITAYEZU Jean Pierre <123136885+Lapidis2@users.noreply.github.com>
2025-02-09 23:37:54 +0200

    Update challengeModal.ts

--- src/Models/challengeModal.ts
+++ src/Models/challengeModal.ts
@@ -1,26 +1,38 @@
 import mongoose from "mongoose";
-
-
 interface blogModal{
     title:String,
-    skills:String,
-    seniority:String,
-    status: String,
-    timeline:String
+    headline:String,
+    content:String,
+    imageUrl:String,
+    views:[],
+    likes:[],
+    shares:[],
+    comment:[{
+        author:mongoose.Types.ObjectId,
+        content:String,
+         date  :Date
+    }],
+    createdAt: Date 
 }
-
-const blogSchema = new mongoose.Schema(
-  {
-    title: { type: String, required: true },
-    skills: { type: [String], required: true },
-    seniority: { type: String, required: true },
-    timeline: { type: String },
-    status: { type: String },
-  },
-  {
-    timestamps: true,
-  }
-);
-
+const blogSchema= new mongoose.Schema<blogModal>({
+         title:{type:String},
+         headline:{type:String},
+         content:{type:String},
+         imageUrl:{type:String},
+         createdAt:{type:Date,default: Date.now()},
+         likes:[{type:mongoose.Types.ObjectId,ref:"User"}],
+        comment: [
+            {
+                  author:String,
+                
+                content: {type:String
+                         
+                     },
+                created: { type: Date, default: Date.now },
+            },
+        ],
+    },
+    { timestamps: true 
+    })
 const blogModal=mongoose.model("blogs",blogSchema)
 export default blogModal
