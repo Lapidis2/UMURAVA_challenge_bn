250a2092b2edb29c17b702d5def1ea396d59ce44
HITAYEZU Jean Pierre <123136885+Lapidis2@users.noreply.github.com>
2025-02-09 22:33:23 +0200

    Update challengeModal.ts

--- src/Models/challengeModal.ts
+++ src/Models/challengeModal.ts
@@ -5,15 +5,6 @@ interface blogModal{
     title:String,
     headline:String,
     content:String,
-    imageUrl:String,
-    views:[],
-    likes:[],
-    shares:[],
-    comment:[{
-        author:mongoose.Types.ObjectId,
-        content:String,
-         date  :Date
-    }],
     createdAt: Date 
 }
 
@@ -21,22 +12,8 @@ const blogSchema= new mongoose.Schema<blogModal>({
          title:{type:String},
          headline:{type:String},
          content:{type:String},
-         imageUrl:{type:String},
          createdAt:{type:Date,default: Date.now()},
-         likes:[{type:mongoose.Types.ObjectId,ref:"User"}],
-    
-        comment: [
-            {
-                  author:String,
-                
-                content: {type:String
-                         
-                     },
-                created: { type: Date, default: Date.now },
-                
-              
-            },
-        ],
+         
     },
     { timestamps: true 
     })
