5d1a4a737d1b257181655f35808a3801692675eb
Lapidis2 <hitlapidis2@gmail.com>
2025-03-07 01:10:30 +0200

    add confirm email route

--- src/Controllers/userController.ts
+++ src/Controllers/userController.ts
@@ -58,7 +58,7 @@ export const registerUser = async (req: Request, res: Response) => {
             },
         });
 
-        const confirmationLink = `http://${req.headers.host}/confirm/${rawToken}`;
+        const confirmationLink = `http://${req.headers.host}/api/confirm/${rawToken}`;
 
         const sendEmailResponse = await transporter.sendMail({
             from: process.env.ADMIN_EMAIL,
